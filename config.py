fbb0c1b96723743804af1b30bdab71732ea3d682
nckmrrtt <nicholasjmerritt@gmail.com>
2015-06-06 13:16:50 -0500

    configerate

--- config.py
+++ config.py
@@ -8,30 +8,24 @@ Good luck, cheaters.
 
 """
 
-import items_lists
+from items_lists import random_weapon
 import hashlib
 
 def config():
     if confus(unconfus('bob')) == 'bob':
         print '<secrets configured successfully>'
 
-def confus(stringo):
-    new = ''
-    for item in stringo:
-        new += chr(item-17)
-    return new
+def confus(b):
+    return ''.join([chr(a-17) for a in b])
 
-def unconfus(listo):
-    new = []
-    for item in listo:
-        new.append(ord(item)+17)
-    return new
+def unconfus(a):
+    return [ord(b)+17 for b in a]
 
 
 if __name__ != '__main__':
 
     config0 = [126, 118, 131, 125, 122, 127, 131, 128, 115, 128, 133]
-    config1 = items_lists.random_weapon(confus([115, 128, 132, 132, 112, 136, 118, 114, 129, 128, 127, 132]))
+    config1 = random_weapon(confus([115, 128, 132, 132, 112, 136, 118, 114, 129, 128, 127, 132]))
     config2 = [129, 125, 114, 116, 122, 117, 128]
     config3 = [128, 138, 238, 119, 111]
     config4 = [132, 122, 137, 129, 114, 116, 124, 133, 128, 120, 128]
@@ -44,9 +38,8 @@ if __name__ != '__main__':
     ha3 = '76a61453ff19529bd4f230b688f1f416'
 
 
-
 if __name__ == '__main__':
-    
-   for i in range(1):
-       yolo = raw_input()
-       print hashlib.md5(yolo).hexdigest()
+
+    config()
+    for i in range(1):
+        print hashlib.md5(raw_input()).hexdigest()
